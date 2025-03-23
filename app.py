d4ebbb8062977bae04ef7013926d9c1886d92fb6
avipsa2 <125963153+avipsa2@users.noreply.github.com>
2025-03-23 16:06:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import pickle
 import numpy as np
-model = pickle.load(open('model.pkl', 'rb'))
+model = pickle.load(open('new_model.pkl', 'rb'))
 
 st.title("Predictive Maintenance System")
 
